b5338dfc119f644f001139193959703984146243
runner <150560315+0xff3232@users.noreply.github.com>
2024-03-02 01:50:52 +0000

    MooBuzz

--- main.go
+++ main.go
@@ -1,9 +1,58 @@
+// https://usaco.org/index.php?page=viewproblem2&cpid=966
+
 package main
 
 import (
 	"fmt"
+	"time"
 )
 
+func LinearMooBuzz(n int) int {
+
+	r := []int{}
+	i := 1
+	for len(r) < n {
+		if i%3 == 0 || i%5 == 0 {
+			i++
+			continue
+		} else {
+			r = append(r, i)
+			i++
+		}
+	}
+	fmt.Println(r)
+
+	return r[len(r)-1]
+}
+
+func isValid(n int, x int) bool {
+	return x-(x/3+x/5-x/15) >= n
+}
+
+func BMooBuzz(n int) int {
+	left, right := 0, 10000000000
+
+	for left < right {
+		mid := (left + right) / 2
+		if isValid(n, mid) {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	num := 100
+
+	// LinearMooBuzz
+	start := time.Now()
+	fmt.Println("LinearMooBuzz:", LinearMooBuzz(num))
+	fmt.Println("Time taken for LinearMooBuzz:", time.Since(start))
+
+	// BMooBuzz
+	start = time.Now()
+	fmt.Println("BMooBuzz:", BMooBuzz(num))
+	fmt.Println("Time taken for BMooBuzz:", time.Since(start))
 }
