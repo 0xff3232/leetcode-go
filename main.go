d7bb3efae50ee4add7cd42d7a82c405184949abd
runner <150560315+0xff3232@users.noreply.github.com>
2024-03-02 03:17:52 +0000

    700. Search in a Binary Search Tree

--- main.go
+++ main.go
@@ -1,58 +1,98 @@
-// https://usaco.org/index.php?page=viewproblem2&cpid=966
+// https://leetcode.com/problems/search-in-a-binary-search-tree/description/
 
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
-func LinearMooBuzz(n int) int {
-
-	r := []int{}
-	i := 1
-	for len(r) < n {
-		if i%3 == 0 || i%5 == 0 {
-			i++
-			continue
-		} else {
-			r = append(r, i)
-			i++
-		}
-	}
-	fmt.Println(r)
-
-	return r[len(r)-1]
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
 }
 
-func isValid(n int, x int) bool {
-	return x-(x/3+x/5-x/15) >= n
+func NewTreeNode(val int) *TreeNode {
+    return &TreeNode{
+        Val: val,
+        Left: nil,
+        Right: nil, 
+    }
 }
 
-func BMooBuzz(n int) int {
-	left, right := 0, 10000000000
+// Insert for BTS.
+func (bt *TreeNode) Insert(val int) *TreeNode {
+    if bt == nil {
+        return NewTreeNode(val)
+    }
+    if val < bt.Val {
+        bt.Left = bt.Left.Insert(val)
+    } else {
+        bt.Right = bt.Right.Insert(val)
+    }
+    return bt 
+}
 
-	for left < right {
-		mid := (left + right) / 2
-		if isValid(n, mid) {
-			right = mid
-		} else {
-			left = mid + 1
-		}
-	}
-	return left
+// BTS print, meaning in order of Binary Search.
+func (bt *TreeNode) Print() {
+    if bt == nil {
+        return
+    }
+    bt.Left.Print()
+    fmt.Println(bt.Val)
+    bt.Right.Print()
+}
+
+func (bt *TreeNode) Search(target int) *TreeNode{
+    if bt == nil {
+        return nil 
+    }
+
+    if (target < bt.Val) {
+        return bt.Left.Search(target)
+    } else if (target > bt.Val) {
+        return bt.Right.Search(target)
+    } 
+    return bt
+    
 }
 
 func main() {
-	num := 100
-
-	// LinearMooBuzz
-	start := time.Now()
-	fmt.Println("LinearMooBuzz:", LinearMooBuzz(num))
-	fmt.Println("Time taken for LinearMooBuzz:", time.Since(start))
-
-	// BMooBuzz
-	start = time.Now()
-	fmt.Println("BMooBuzz:", BMooBuzz(num))
-	fmt.Println("Time taken for BMooBuzz:", time.Since(start))
+    // creating the cases
+    var root *TreeNode
+    nums := []int{4, 2, 7, 1, 3}
+    for i := range nums {
+        root = root.Insert(nums[i])
+    }
+    root.Print()
+    s := root.Search(7)
+    if s != nil {
+        fmt.Println("target found", s.Val) 
+    } else {
+        fmt.Println("target not found")
+    }
 }
+
+
+/*
+LeetCode struct and solution:
+
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+   if root == nil {
+       return nil
+   } 
+
+   if (val < root.Val) {
+       return searchBST(root.Left, val)
+   } else if (val > root.Val) {
+       return searchBST(root.Right, val)
+   } 
+   return root
+}
+*/
